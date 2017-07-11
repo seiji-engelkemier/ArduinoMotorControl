928f7d45fedf71ce7dfcd50c69a47448f2f5c1c5
Seiji Engelkemier <seiji.engelkemier@gmail.com>
2017-07-11 16:11:46 +0300

    added comments

--- threemotors/threemotors.ino
+++ threemotors/threemotors.ino
@@ -18,144 +18,149 @@ bool enm[3];
 bool s_en[3];
 bool s_dir[3];
 
+// clkp and clkm control step pulse
+// cwp and cwm control rotation direction
 
 
 void setup()
 {
-Serial.begin(115200);
-for (int i=0;i<3;i++)
-{
-  pinMode(p_cwp[i], OUTPUT);
-  pinMode(p_cwm[i], OUTPUT);
-  pinMode(p_clkp[i], OUTPUT);
-  pinMode(p_clkm[i], OUTPUT);
-  pinMode(p_enp[i], OUTPUT);
-  pinMode(p_enm[i], OUTPUT);
+	// Set data transfer rate
+	Serial.begin(115200);
 
-  s_en[i]=1;
-  s_dir[i]=1;
+	// Set pins to OUTPUT mode
+	for (int i=0;i<3;i++)
+	{
+	  pinMode(p_cwp[i], OUTPUT);
+	  pinMode(p_cwm[i], OUTPUT);
+	  pinMode(p_clkp[i], OUTPUT);
+	  pinMode(p_clkm[i], OUTPUT);
+	  pinMode(p_enp[i], OUTPUT);
+	  pinMode(p_enm[i], OUTPUT);
 
-}
+	  s_en[i]=1;
+	  s_dir[i]=1;
 
-pinMode(13, OUTPUT);
+	}
 
-for (int i=0;i<3;i++)
-{
-  cwp[i]=LOW;
-  cwm[i]=LOW;
-  clkp[i]=LOW;
-  clkm[i]=LOW;
-  enp[i]=LOW;
-  enm[i]=LOW;
+	pinMode(13, OUTPUT);
+
+	for (int i=0;i<3;i++)
+	{
+	  cwp[i]=LOW;
+	  cwm[i]=LOW;
+	  clkp[i]=LOW;
+	  clkm[i]=LOW;
+	  enp[i]=LOW;
+	  enm[i]=LOW;
  
-  digitalWrite(p_cwp[i],cwp[i]);
-  digitalWrite(p_cwm[i],cwm[i]);
-  digitalWrite(p_clkp[i],clkp[i]);
-  digitalWrite(p_clkm[i],clkm[i]);
-  digitalWrite(p_enp[i],enp[i]);
-  digitalWrite(p_enm[i],enm[i]);
-}
+	  digitalWrite(p_cwp[i],cwp[i]);
+	  digitalWrite(p_cwm[i],cwm[i]);
+	  digitalWrite(p_clkp[i],clkp[i]);
+	  digitalWrite(p_clkm[i],clkm[i]);
+	  digitalWrite(p_enp[i],enp[i]);
+	  digitalWrite(p_enm[i],enm[i]);
+	}
 
 
-digitalWrite(13, LOW);
+	digitalWrite(13, LOW);
 
 }
 
 void loop() {
 
-for (int i=0;i<3;i++)
-{
-  if (s_en[i]==1)
-  {
-    if (s_dir[i]==1)
-    {
-      cwp[i]=HIGH;
-      cwm[i]=LOW;
-      clkp[i]=HIGH;
-      clkm[i]=LOW;
-    }
-    if (s_dir[i]==0)
-    {
-      cwp[i]=LOW;
-      cwm[i]=HIGH;
-      clkp[i]=LOW;
-      clkm[i]=HIGH;
-    }  
+	for (int i=0;i<3;i++)
+	{
+	  if (s_en[i]==1)
+	  {
+		if (s_dir[i]==1)
+		{
+		  cwp[i]=HIGH;
+		  cwm[i]=LOW;
+		  clkp[i]=HIGH;
+		  clkm[i]=LOW;
+		}
+		if (s_dir[i]==0)
+		{
+		  cwp[i]=LOW;
+		  cwm[i]=HIGH;
+		  clkp[i]=LOW;
+		  clkm[i]=HIGH;
+		}  
   
-  digitalWrite(p_clkp[i],clkp[i]);
-  digitalWrite(p_clkm[i],clkm[i]); 
-  digitalWrite(p_cwp[i],cwp[i]);
-  digitalWrite(p_cwm[i],cwm[i]);
-
-  
-  }
+	  digitalWrite(p_clkp[i],clkp[i]);
+	  digitalWrite(p_clkm[i],clkm[i]); 
+	  digitalWrite(p_cwp[i],cwp[i]);
+	  digitalWrite(p_cwm[i],cwm[i]);
 
   
-}
-
-
-
-
-digitalWrite(13, HIGH);
-delayMicroseconds(delayms);
-
-for (int i=0;i<3;i++)
-{
-  if (s_en[i]==1)
-  {
-    if (s_dir[i]==1)
-    {
-      cwp[i]=HIGH;
-      cwm[i]=LOW;
-      clkp[i]=LOW;
-      clkm[i]=LOW;
-    }
-    if (s_dir[i]==0)
-    {
-      cwp[i]=LOW;
-      cwm[i]=HIGH;
-      clkp[i]=LOW;
-      clkm[i]=LOW;
-    }  
-  
-  digitalWrite(p_clkp[i],clkp[i]);
-  digitalWrite(p_clkm[i],clkm[i]); 
-  digitalWrite(p_cwp[i],cwp[i]);
-  digitalWrite(p_cwm[i],cwm[i]);
+	  }
 
   
-  }
+	}
+
+
+
+
+	digitalWrite(13, HIGH);
+	delayMicroseconds(delayms);
+
+	for (int i=0;i<3;i++)
+	{
+	  if (s_en[i]==1)
+	  {
+		if (s_dir[i]==1)
+		{
+		  cwp[i]=HIGH;
+		  cwm[i]=LOW;
+		  clkp[i]=LOW;
+		  clkm[i]=LOW;
+		}
+		if (s_dir[i]==0)
+		{
+		  cwp[i]=LOW;
+		  cwm[i]=HIGH;
+		  clkp[i]=LOW;
+		  clkm[i]=LOW;
+		}  
+  
+	  digitalWrite(p_clkp[i],clkp[i]);
+	  digitalWrite(p_clkm[i],clkm[i]); 
+	  digitalWrite(p_cwp[i],cwp[i]);
+	  digitalWrite(p_cwm[i],cwm[i]);
 
   
-}
+	  }
 
-//digitalWrite(22,LOW); 
+  
+	}
 
-digitalWrite(13, LOW);
+	//digitalWrite(22,LOW); 
 
-//delay(1000);
-delayMicroseconds(delayms);
+	digitalWrite(13, LOW);
 
-
-//Communication part
-String rec=readserial();
-
-if (rec!="")
-{
-  for (int i=0;i<3;i++)
-  {
-    s_en[i]=(getValue(rec, ':', i)).toInt();
-  }
-  for (int i=3;i<6;i++)
-  {
-    s_dir[i]=(getValue(rec, ':', i)).toInt();
-  }
-  delayms=(getValue(rec, ':', 6)).toInt();
-}
+	//delay(1000);
+	delayMicroseconds(delayms);
 
 
+	//Communication part
+	String rec=readserial();
 
-}
+	if (rec!="")
+	{
+	  for (int i=0;i<3;i++)
+	  {
+		s_en[i]=(getValue(rec, ':', i)).toInt();
+	  }
+	  for (int i=3;i<6;i++)
+	  {
+		s_dir[i]=(getValue(rec, ':', i)).toInt();
+	  }
+	  delayms=(getValue(rec, ':', 6)).toInt();
+	}
+
+
+	// End of loop
+} 
 
 
 String readserial()
