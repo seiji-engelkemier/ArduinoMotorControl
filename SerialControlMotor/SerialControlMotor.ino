db9b00ed5a541b478d209d740ede782e0b850afc
Seiji Engelkemier <seiji.engelkemier@gmail.com>
2017-07-13 13:35:18 +0300

    button control working

--- SerialControlMotor/SerialControlMotor.ino
+++ SerialControlMotor/SerialControlMotor.ino
@@ -1,4 +1,4 @@
-int delayms=100;
+int delayms=50;
 
 // 6 pin numbers for each motor
 int p_clkp[]={22,32,42};
@@ -18,14 +18,22 @@ bool enm[3];
 bool s_en[3];
 bool s_dir[3];
 
+// Button control 
+const int greenBut=9;
+const int redBut=8;
+
 
 
 void setup()
 {
   // Set data transfer rate
-  Serial.begin(115200);
+  Serial.begin(9600);
 
-  // Set each pin to OUTPUT mode
+  // Set the pins for buttons to Inputs
+  pinMode(greenBut, INPUT);
+  pinMode(redBut, INPUT);
+
+  // Set each motor pin to OUTPUT mode
   for (int i=0;i<3;i++)
   {
     pinMode(p_cwp[i], OUTPUT);
@@ -55,175 +63,74 @@ void setup()
   }
 
   
-  Serial.println("Enter a to rotate motor CW, press z for CCW, q to stop");
+//  Serial.println("Enter a to rotate motor CW, press z for CCW, q to stop");
   
 }
 
 
 void loop() 
 {
-  int i = 0;
-  if (Serial.available() > 0)
+  if (digitalRead(greenBut) == LOW)
   {
-    int UserInput = Serial.read();
-    if (UserInput == 122) // 122 corresponds to z
+    Serial.println("green Button pressed");
+    // Run all motors CCW
+    for (int i=0;i<3;i++)
     {
-      Serial.println("you pressed z");
-      delay(500);
-      // Spin CCW
-      for (int j=0;j<1000;j++)
-      {
-        cwp[0]=HIGH;
-        cwm[0]=LOW;
-        clkp[0]=HIGH;
-        clkm[0]=LOW;
-
-        digitalWrite(p_clkp[i],clkp[i]);
-        digitalWrite(p_clkm[i],clkm[i]); 
-        digitalWrite(p_cwp[i],cwp[i]);
-        digitalWrite(p_cwm[i],cwm[i]);
-        
-        delayMicroseconds(delayms);
-
-        clkp[0]=LOW;
-        clkm[0]=LOW;
-        
-        digitalWrite(p_clkp[0],clkp[0]);
-        digitalWrite(p_clkm[0],clkm[0]); 
-
-        delayMicroseconds(delayms);
-      }
+      cwp[i]=HIGH;
+      cwm[i]=LOW;
+      clkp[i]=HIGH;
+      clkm[i]=LOW;
     }
-    if (UserInput == 97) // 97 corresponds to a
-    {
-      Serial.println("you pressed a");
-      // Spin CW
-      for (int j=0;j<1000;j++)
-      {
-        cwp[0]=LOW;
-        cwm[0]=HIGH;
-        clkp[0]=HIGH;
-        clkm[0]=LOW;
-
-        digitalWrite(p_clkp[0],clkp[0]);
-        digitalWrite(p_clkm[0],clkm[0]); 
-        digitalWrite(p_cwp[0],cwp[0]);
-        digitalWrite(p_cwm[0],cwm[0]);
-        
-        delayMicroseconds(delayms);
-
-        clkp[0]=LOW;
-        clkm[0]=LOW;
-        
-        digitalWrite(p_clkp[0],clkp[0]);
-        digitalWrite(p_clkm[0],clkm[0]); 
-
-        delayMicroseconds(delayms);
-      }
-    } 
-    if (UserInput == 113) // 113 corresponds to z
-    {
-      // 
-      cwp[0]=LOW;
-      cwm[0]=LOW;
-      clkp[0]=LOW;
-      clkm[0]=LOW;
-
-      digitalWrite(p_clkp[i],clkp[i]);
-      digitalWrite(p_clkm[i],clkm[i]); 
-      digitalWrite(p_cwp[i],cwp[i]);
-      digitalWrite(p_cwm[i],cwm[i]);
-    }
-
-//    int i = 0;
-//    digitalWrite(p_clkp[i],clkp[i]);
-//    digitalWrite(p_clkm[i],clkm[i]); 
-//    digitalWrite(p_cwp[i],cwp[i]);
-//    digitalWrite(p_cwm[i],cwm[i]);
-
-    
+    WriteSignal();
+    delayMicroseconds(delayms);
+    MotorOffSignal(); // Includes ending delay timing
   }
+  if (digitalRead(redBut) == LOW)
+  {
+    Serial.println("red Button pressed");
+    // Run all motors CW
+    for (int i=0;i<3;i++)
+    {
+      cwp[i]=LOW;
+      cwm[i]=HIGH;
+      clkp[i]=HIGH;
+      clkm[i]=LOW;
+    }
+    WriteSignal();
+    delayMicroseconds(delayms);
+    MotorOffSignal(); // Includes ending delay timing
+  }
+  
+  
+  
+ 
 
-//  delayMicroseconds(delayms);
-//  
-////  DirValue = Serial.read();
-////  for (int i=0;i<3;i++)
-////  {
-////    s_dir[i]=DirValue;
-////  }
-////  
-////  for (int i=0;i<3;i++)
-////  {
-////    if (s_en[i]==1)
-////    {
-////      if (s_dir[i]==1)
-////      {
-////        // Spin CW
-////        cwp[i]=LOW;
-////        cwm[i]=HIGH;
-////        clkp[i]=HIGH;
-////        clkm[i]=LOW;
-////
-////        if (i == 2) 
-////        {
-////          // Spin CCW
-////          cwp[i]=HIGH;
-////          cwm[i]=LOW;
-////          clkp[i]=HIGH;
-////          clkm[i]=LOW;
-////        }
-////      }
-////      if (s_dir[i]==0)
-////      {
-////        cwp[i]=LOW;
-////        cwm[i]=HIGH;
-////        clkp[i]=LOW;
-////        clkm[i]=HIGH;
-////      }  
-////    
-////    digitalWrite(p_clkp[i],clkp[i]);
-////    digitalWrite(p_clkm[i],clkm[i]); 
-////    digitalWrite(p_cwp[i],cwp[i]);
-////    digitalWrite(p_cwm[i],cwm[i]);
-////    
-////    } 
-////    
-////  }
-////  
-////  
-////  delayMicroseconds(delayms);
-//
-//  // Need this portion to turn clock signal off or else motor doesn't spin.
-//  for (int i=0;i<3;i++)
-//  {
-//    if (s_en[i]==1)
-//    {
-//      if (s_dir[i]==1)
-//      {
-//        cwp[i]=HIGH;
-//        cwm[i]=LOW;
-//        clkp[i]=LOW;
-//        clkm[i]=LOW;
-//      }
-//      if (s_dir[i]==0)
-//      {
-//        cwp[i]=LOW;
-//        cwm[i]=HIGH;
-//        clkp[i]=LOW;
-//        clkm[i]=LOW;
-//      }  
-//    
-//    digitalWrite(p_clkp[i],clkp[i]);
-//    digitalWrite(p_clkm[i],clkm[i]); 
-//    digitalWrite(p_cwp[i],cwp[i]);
-//    digitalWrite(p_cwm[i],cwm[i]);
-//  
-//    
-//    }
-//  
-//    
-//  }
-//  
-//  delayMicroseconds(delayms);
   
 } // end of main loop
+
+void WriteSignal()
+{
+  for (int i=0;i<3;i++)
+  {
+    digitalWrite(p_clkp[i],clkp[i]);
+    digitalWrite(p_clkm[i],clkm[i]); 
+    digitalWrite(p_cwp[i],cwp[i]);
+    digitalWrite(p_cwm[i],cwm[i]);
+  }
+}
+
+void MotorOffSignal()
+{
+  for (int i=0;i<3;i++)
+  {
+    clkp[i]=LOW;
+    clkm[i]=LOW;
+
+    digitalWrite(p_clkp[i],clkp[i]);
+    digitalWrite(p_clkm[i],clkm[i]); 
+  }      
+    
+  delayMicroseconds(delayms);
+}
+
+
